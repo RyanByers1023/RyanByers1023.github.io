ccd8a05e51cde156b26867b89fcdc21a6a296b28
RyanByers1023 <ryanbyers1023@gmail.com>
2026-03-23 19:29:05 +0000

    create: gallery folder, update imports

--- src/ts/projectCardManager/handlers/index.ts
+++ src/ts/projectCardManager/handlers/index.ts
@@ -2,4 +2,4 @@ export { CardStateHandler } from '@handlers/cardStateHandler';
 export { TiltHandler } from '@src/ts/projectCardManager/handlers/cardEffectHandlers/tiltHandler';
 export { FlipHandler } from '@src/ts/projectCardManager/handlers/cardFunctionalityHandlers/flipHandler';
 export { ScrollHandler } from '@src/ts/projectCardManager/handlers/cardFunctionalityHandlers/scrollHandler';
-export { GalleryHandler } from '@src/ts/projectCardManager/handlers/cardFunctionalityHandlers/galleryHandler';
+export { GalleryHandler } from '@src/ts/projectCardManager/handlers/cardFunctionalityHandlers/gallery/galleryHandler';
